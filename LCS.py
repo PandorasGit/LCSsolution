e1984f3dd6d5189391ae6e0a7d69b4eb0fe8c21f
wendallR <wendallar125@gmail.com>
2021-12-13 19:08:34 -0500

    Add matrix node class

--- LCS.py
+++ LCS.py
@@ -1,24 +1,48 @@
 import unittest
 
 
+class MatrixNode:
+    """Node in the matrix used to read the LCS"""
+
+    def __init__(self, direction, length, letter=None):
+        self.direction = direction
+        self.length = length
+        self.letter = letter
+
+
 class LCS:
 
     def __init__(self, first_string, second_string):
-        self.first_string = first_string
-        self.second_string = second_string
+        def determine_longer_string():
+            """Decide which string should be considered the first string regardless of entry"""
+            if len(first_string) < len(second_string):
+                self.first_string = first_string
+                self.second_string = second_string
+            else:
+                self.first_string = second_string
+                self.second_string = first_string
+
+        determine_longer_string()
+        self.solution_matrix = [[0 for x in range(len(self.second_string)+1)] for x in range(len(self.first_string)+1)]
 
     def lcs(self):
         """Calculate the length of an LCS"""
+
         def lcs_recursive(index_of_first_string, index_of_second_string):
             """Recursive call to loop calculate length"""
             if index_of_first_string == 0 or index_of_second_string == 0:
                 return 0
-            if self.first_string[index_of_first_string-1] == self.second_string[index_of_second_string-1]:
-                return lcs_recursive(index_of_first_string-1, index_of_second_string-1) + 1
+            if self.first_string[index_of_first_string - 1] == self.second_string[index_of_second_string - 1]:
+                matrix_node = MatrixNode("up right",
+                                         lcs_recursive(index_of_first_string - 1, index_of_second_string - 1) + 1,
+                                         self.first_string[index_of_first_string - 1])
+                self.solution_matrix[index_of_first_string-1][index_of_second_string-1] = matrix_node
+                return matrix_node.length
             return max(
-                lcs_recursive(index_of_first_string, index_of_second_string-1),
-                lcs_recursive(index_of_first_string-1, index_of_second_string)
+                lcs_recursive(index_of_first_string, index_of_second_string - 1),
+                lcs_recursive(index_of_first_string - 1, index_of_second_string)
             )
+
         return lcs_recursive(len(self.first_string), len(self.second_string))
 
 
@@ -26,6 +50,7 @@ class TestClass(unittest.TestCase):
     """
     Class for testing LCS Strings
     """
+
     def test_lcs_length_of_empty_strings(self):
         string_x = ""
         string_y = ""
@@ -49,6 +74,7 @@ class TestClass(unittest.TestCase):
         string_y = "slizudjfg"
         lcs = LCS(string_x, string_y)
         self.assertEqual(3, lcs.lcs())
+
 
 
 def main():
